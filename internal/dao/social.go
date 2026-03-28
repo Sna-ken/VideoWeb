30b1f3385a2335685d5b2bbe48753727ef93226e
Snaken <1339440756@qq.com>
2026-03-29 00:35:17 +0800

    fix problem

--- internal/dao/social.go
+++ internal/dao/social.go
@@ -29,9 +29,9 @@ func FindFollowByUserID(ctx context.Context, userID string, offset int, pagesize
 }
 
 func FindFollowerByUserID(ctx context.Context, userID string, offset int, pagesize int, object *[]SocialObject) error {
-	return config.MYSQLDB.WithContext(ctx).Table("social_object").Select("users.id as user_id, users.username,users.avatar_url").
-		Joins("JOIN users ON social_object.user_id = users.id").
-		Where("social_object.object_id = ?", userID).Offset(offset).Limit(pagesize).Find(object).Error
+	return config.MYSQLDB.WithContext(ctx).Table("social_objects").Select("users.id as user_id, users.username,users.avatar_url").
+		Joins("JOIN users ON social_objects.user_id = users.id").
+		Where("social_objects.object_id = ?", userID).Offset(offset).Limit(pagesize).Find(object).Error
 }
 
 func FindFriendByUserID(ctx context.Context, userID string, offset int, pagesize int, object *[]SocialObject) error {
