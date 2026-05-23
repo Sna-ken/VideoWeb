96e9cb23414981f6ac29f4df59aa35b05dfeba0f
Snaken <1339440756@qq.com>
2026-05-23 23:25:29 +0800

    feat:ai聊天

--- internal/dao/chat.go
+++ internal/dao/chat.go
@@ -2,9 +2,11 @@ package dao
 
 import (
 	"context"
+	"time"
 
 	"github.com/Sna-ken/videoweb/config"
 	"github.com/Sna-ken/videoweb/internal/model"
+	"github.com/redis/go-redis/v9"
 )
 
 func CreateMessage(ctx context.Context, msg *model.Message) error {
@@ -25,4 +27,34 @@ func FindMessageByRedis(ctx context.Context, rediskey string, start int64, end i
 
 func FindMessageByMysql(ctx context.Context, messages *[]*model.Message, limit int) error {
 	return config.MYSQLDB.WithContext(ctx).Order("create_at DESC").Limit(limit).Find(&messages).Error
+}
+
+func SetTimer(ctx context.Context, key string, duration time.Duration) error {
+	return config.REDISDB.Set(ctx, key, 1, duration).Err()
+}
+
+func DeleteTimer(ctx context.Context, key string) error {
+	return config.REDISDB.Del(ctx, key).Err()
+}
+
+func IsTimeout(ctx context.Context, key string) (bool, error) {
+	err := config.REDISDB.Get(ctx, key).Err()
+	if err == redis.Nil {
+		return true, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return false, nil
+}
+
+func IsRateLimited(ctx context.Context, key string) (bool, error) {
+	err := config.REDISDB.Get(ctx, key).Err()
+	if err == redis.Nil {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
